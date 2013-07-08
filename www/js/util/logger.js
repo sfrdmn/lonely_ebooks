38b1c54eb388b25764a37cfb70ee9d00362324a2
Sean Fridman <fridman@mail.sfsu.edu>
2013-07-07 17:32:20 -0700

    Using my own logger class

--- www/js/util/logger.js
+++ www/js/util/logger.js
@@ -1,7 +1,22 @@
-var loggingEnabled = require('../../settings.json').logging
+var logLevel = require('../../settings.json').logLevel
+var _ = require('lodash')
 
-module.exports = {
-	info: function() { console.log.apply(console, arguments); },
-	error: function() { console.log.apply(console, arguments); },
-	log: function() { console.log.apply(console, arguments); }
-}
+// priority moves from left to right
+// debug becomes easy way to quickly spew stuff out into otherwise silent console
+
+var levels = ['debug', 'error', 'warn', 'info', 'log']
+var levelMap = {}
+var fallback = console.log || function() {}
+
+_.each(levels, function(level, i) {
+	levelMap[level] = i + 1
+
+	if (typeof console[level] !== 'function') {
+		console[level] = fallback
+	}
+
+	exports[level] = function() {
+		if (logLevel >= levelMap[level])
+			console[level].apply(console, arguments); 
+	}
+})
