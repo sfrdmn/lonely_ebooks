b96b72028b230d5d0e23c2744e519b823bdd443a
Sean Fridman <fridman@mail.sfsu.edu>
2013-07-25 17:08:38 -0700

    Wrote a simple logger with transport support

---
+++ www/js/src/logger.js
@@ -0,0 +1,75 @@
+/**
+ * Can't a guy get a simple transport logging system around here?
+ */
+
+var inherits = require('util').inherits
+
+var levels = {error: 0, warn: 1, info: 2, verbose: 3, silly: 4}
+
+function Logger(options) {
+  this.configure(options)
+}
+
+Logger.prototype.configure = function (options) {
+  options = options || {}
+  if (options.transports && options.transports.length) {
+    options.transports.forEach(function(transport) {
+      this.addTransport(transport)
+    })
+  } else {
+    this.addTransport('console')
+  }
+  this.logLevel = options.level || 3
+}
+
+Logger.prototype.addTransport = function (transport) {
+  if (!this._transports)
+    this._transports = []
+  if (transports[transport])
+    this._transports.push(new transports[transport])
+  else
+    (new ConsoleTransport).log('warn', 'Bad transport specified in logger')
+}
+
+Logger.prototype._transport = function (level, msgs) {
+  this._transports.forEach(function(transport) {
+    transport.log(level, msgs)
+  })
+}
+
+Object.keys(levels).forEach(function(level) {
+  Logger.prototype[level] = function() {
+    var msgs = [].slice.call(arguments, 0)
+    this._transport(level, msgs)
+  }
+})
+
+function ConsoleTransport() {
+  this._console = console || {}
+  this._console.log = (typeof this._console.log === 'function' &&
+      this._console.log) || function() {}
+  this._headers = ConsoleTransport.headers
+}
+
+ConsoleTransport.prototype.log = function(level, msgs) {
+  if (this._headers[level])
+    this._console.log.apply(this._console, this._headers[level].concat(msgs))
+  else
+    this.log('warn', 'Bad log method for ConsoleTransport')
+}
+
+ConsoleTransport.headers = {
+  'error': ['%c error   ', 'background-color: #e10; color: #eee'],
+  'warn': ['%c warn    ', 'background-color: #ea0; color: #eee'],
+  'info': ['%c info    ', 'background-color: #06a; color: #eee'],
+  'verbose': ['%c verbose ', 'background-color: #d40; color: #eee'],
+  'silly': ['%c silly   ', 'background-color: #a4a; color: #eee'],
+}
+
+var transports = {
+  console: ConsoleTransport
+}
+
+module.exports = new Logger
+module.exports.Logger = Logger
+module.exports.transports = transports
