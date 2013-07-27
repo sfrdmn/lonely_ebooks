a1d486e26a1d6a5bd169532a1c129382e471683a
Sean Fridman <fridman@mail.sfsu.edu>
2013-07-26 20:11:07 -0700

    Lil demo with a bunch of random images

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,19 @@
-var ResourceMananger = require('./src/resource-manager.js')
-var ArtifactManager = require('./src/artifact-manager.js')
+var amanager = require('./src/artifact-manager.js')
 var createjs = require('createjs')
+var logger = require('./src/logger.js')
 
 function App() {
-  this.am = new ArtifactManager()
-  this.am.fetch(10)
+  amanager.fetch(50)
+  amanager.on('ready', (function() {
+    var artifact = amanager.next()
+    var bitmap = new createjs.Bitmap(artifact.image)
+    stage.addChild(bitmap)
+    stage.update()
+  }).bind(this))
 }
+
+var canvas = document.getElementById('viewport')
+var stage = new createjs.Stage(canvas)
+
 
 exports.App = App
