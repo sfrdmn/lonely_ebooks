d54f464ecc688132495944c9f0f9990391a95591
Sean Fridman <fridman@mail.sfsu.edu>
2013-07-26 20:09:38 -0700

    ResourceManager no longer depends on PreloadJS

--- www/js/src/artifact-manager.js
+++ www/js/src/artifact-manager.js
@@ -6,6 +6,7 @@ var CouchClient = require('./couch-client.js')
 var EventEmitter = require('events').EventEmitter
 var logger = require('./logger.js')
 var inherits = require('util').inherits
+var rmanager = require('./resource-manager.js')
 
 function ArtifactManager() {
   this.couch = new CouchClient({
@@ -23,29 +24,49 @@ ArtifactManager.prototype.fetch = function(count) {
   this.couch.view(couchSettings.ddoc, 'random', {
     startkey: Math.random(),
     limit: count
-  }, function(err, data) {
+  }, function(err, artifacts) {
     if (err) {
       logger.error('Error fetching artifacts', err)
     } else {
-      logger.info('Fetched artifacts!', data)
-      self.enqueue(data)
+      logger.info('Fetched artifacts!', artifacts)
+      self._process(artifacts)
     }
   })
 }
 
-ArtifactManager.prototype.enqueue = function(artifact) {
-  if (Array.isArray(artifact)) {
-    this._queue.concat(artifact)
-  } else {
-    this.push(artifact)
+ArtifactManager.prototype._process = function(artifacts) {
+  if (Array.isArray(artifacts)) {
+    rmanager.loadRemoteImages(artifacts.map(function(artifact) {
+      return {
+        src: artifact.url,
+        id: artifact._id,
+        data: artifact
+      }
+    }), onLoad.bind(this), onDone.bind(this))
   }
-  this.emit('ready')
+
+  function onLoad(err, type, result, data) {
+    if (err) {
+      logger.warn('Could not load artifact image', err, data)
+    } else {
+      data.image = result
+      this._queue.push(data)
+      this.emit('ready')
+      logger.verbose('Loaded artifact image', result)
+    }
+  }
+
+  function onDone(err, results) {
+    logger.verbose('Done loading artifact images')
+  }
 }
 
 ArtifactManager.prototype.next = function() {
   if (this._queue.length) {
     return this._queue.shift()
+  } else {
+    logger.warn('Tried to fetch from empty queue')
   }
 }
 
-module.exports = ArtifactManager
+module.exports = new ArtifactManager
--- www/js/src/resource-manager.js
+++ www/js/src/resource-manager.js
@@ -1,16 +1,59 @@
-var createjs = require('createjs')
+var async = require('async')
 
 function ResourceManager() {
-  this.queue = createjs.LoadQueue()
 }
 
-ResourceManager.prototype.loadAll = function loadAll(resources, callback, load) {
-  var _resources = resources || {}
-  var _callback = typeof callback === 'function' ? callback : function() {}
-  var _load = typeof load === 'undefined' ? true : load
-
-  this.queue.loadManifest(_resources, _load)
-  this.queue.addEventListener('complete', callback)
+ResourceManager.prototype.loadAll = function (resources, callback, load) {
 }
 
-module.exports = ResourceManager
+/**
+ * @param images {Array.<{id: string, src: string, type: string, data}>}
+ * @param load {ImageLoadCallback}
+ * @param done {ImagesDoneCallback}
+ */
+/**
+ * @callback ImageLoadCallback
+ * @param err {Object}
+ * @param type {string}
+ * @param result {Object|Element}
+ * @param data {Object}
+ */
+/**
+ * @callback ImageDoneCallback
+ * @param err {Object}
+ * @param results {Array.<{Object|Element}>}
+ */
+ResourceManager.prototype.loadRemoteImages = function(imageData, load, done) {
+  imageData = imageData || []
+  load = typeof load === 'function' ? load : function() {}
+  done = typeof done === 'function' ? done : function() {}
+
+  var results = []
+  var queue = async.queue(function(imageData, callback) {
+    var image = new Image()
+    image.src = imageData.src
+    image.onload = onImageLoad
+    image.onerror = onImageError
+
+    function onImageLoad() {
+      results.push(image)
+      load(null, imageData.type, image, imageData.data)
+      callback()
+    }
+
+    function onImageError(err) {
+      err = err || new Error('Failed to load image')
+      load(err)
+      callback(err)
+    }
+  }, imageData.length)
+
+  queue.drain = onDrain
+  queue.push(imageData)
+
+  function onDrain() {
+    done(null, results)
+  }
+}
+
+module.exports = new ResourceManager
